786bb386f36849d9bdccb46f30b94983ca9a9f73
ThiagoFirmeza <102990850+ThiagoFirmeza@users.noreply.github.com>
2024-06-24 14:39:40 -0300

    app.js

--- app.js
+++ app.js
@@ -1,55 +1,31 @@
-//Função para calcular o IMC 
-function calcularIMC(peso, altura) {
-    const imc = peso / (altura * altura);
-    const classificacao = classificarIMC(imc);
-    return { imc, classificacao };
-}
+function calcularIMC() {
+    var peso = document.getElementById('divPeso').querySelector('input').value;
+    var altura = document.getElementById('divAltura').querySelector('input').value;
+    var resultado = document.getElementById('resultado');
 
-//Função para classificar o IMC
-function classificarIMC(imc) {
-    if (imc < 18.5) {
-        return "Abaixo do peso";
-    } else if (imc < 25) {
-        return "Peso normal";
-    } else if (imc < 30) {
-        return "Sobrepeso";
+    if (peso === '' || altura === '') {
+        alert('Por favor, preencha os campos de peso e altura.');
     } else {
-        return "Obeso";
+        var imc = peso / (altura * altura);
+
+        if (imc < 18.5) {
+            resultado.style.backgroundColor = '#9C27B0';
+            resultado.innerText = 'Abaixo do Peso: ' + imc.toFixed(2);
+        } else if (imc >= 18.5 && imc < 24.9) {
+            resultado.style.backgroundColor = '#4CAF50';
+            resultado.innerText = 'Peso Normal: ' + imc.toFixed(2);
+        } else if (imc >= 25 && imc < 29.9) {
+            resultado.style.backgroundColor = '#FFC107';
+            resultado.innerText = 'Sobrepeso: ' + imc.toFixed(2);
+        } else if (imc >= 30 && imc < 34.9) {
+            resultado.style.backgroundColor = '#FF9800';
+            resultado.innerText = 'Obesidade Grau I: ' + imc.toFixed(2);
+        } else if (imc >= 35 && imc < 39.9) {
+            resultado.style.backgroundColor = '#FF5722';
+            resultado.innerText = 'Obesidade Grau II: ' + imc.toFixed(2);
+        } else {
+            resultado.style.backgroundColor = '#795548';
+            resultado.innerText = 'Obesidade Grau III (Obesidade Mórbida): ' + imc.toFixed(2);
+        }
     }
 }
-
-//Array com os dados dos pacientes
-let arrayNomes = ['Thiago', 'Leticia', 'Fernando', 'Talizze', 'Guilherme', 'Victor', 'João', 'Luiz', 'Ryan', 'Enzo', 'Maria Eduarda', 'Ana Luiza', 'Theo', 'Maria Luiza', 'Alice'];
-let arrayPesos = [52, 57, 75, 60, 62, 65, 65, 100, 78, 70, 58, 50, 48, 45, 53];
-let arrayAlturas = [1.60, 1.59, 1.64, 1.52, 1.65, 1.75, 1.65, 1.80, 1.63, 1.70, 1.60, 1.58, 1.58, 1.55, 1.62];
-let arrayResultados = [];
-
-//For para calcular o IMC de cada paciente e armazenar
-for (let x = 0; x < arrayNomes.length; x++) {
-    const nome = arrayNomes[x];
-    const peso = arrayPesos[x];
-    const altura = arrayAlturas[x];
-    const { imc, classificacao } = calcularIMC(peso, altura);
-
-    arrayResultados.push({
-        nome,
-        peso,
-        altura,
-        imc: imc.toFixed(2),
-        classificacao,
-    });
-}
-
-//Função para exibir os dados de cada paciente
-function resultados() {
-    arrayResultados.forEach((paciente, indice) => {
-        console.log(`
-            - Nome: ${paciente.nome}
-            - Altura: ${paciente.altura} m
-            - IMC: ${paciente.imc}
-            - Diagnóstico: ${paciente.classificacao}
-        `);
-    });
-}
-
-resultados();
